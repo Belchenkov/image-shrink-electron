3d60f0b5d0b6ccd80df5994348def7748911edcb
Aleksey Belchenkov <u608110@gmail.com>
2020-06-02 17:19:02 +0300

    Add Icons

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ function createMainWindow() {
     mainWindow = new BrowserWindow({
        title: 'ImageShrink',
        width: 500,
-       height: 600
+       height: 600,
+       icon: './assets/icons/Icon_256x256.png'
     });
 
     mainWindow.loadFile(`./app/index.html`);
