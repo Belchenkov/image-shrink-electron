2a67d35fa4456185db413718d6e3aa4551de9cd3
Aleksey Belchenkov <u608110@gmail.com>
2020-06-05 17:05:23 +0300

    Menu & Global Shortcuts

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -14,7 +14,8 @@ function createMainWindow() {
        width: 500,
        height: 600,
        icon: './assets/icons/Icon_256x256.png',
-       resizable: !!isDev
+       resizable: !!isDev,
+       backgroundColor: 'white'
     });
 
     mainWindow.loadFile(`./app/index.html`);
@@ -26,6 +27,14 @@ app.on('ready', () => {
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu);
 
+    globalShortcut.register(
+        'CmdOrCtrl+R',
+        () => mainWindow.reload()
+    );
+    globalShortcut.register(
+        isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+        () => mainWindow.toggleDevTools()
+    );
 
     mainWindow.on('ready', () => mainWindow = null);
 });
@@ -37,6 +46,8 @@ const menu = [
         submenu: [
             {
                 label: 'Quit',
+                //accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+                accelerator: 'CmdOrCtrl+W',
                 click: () => app.quit()
             }
         ]
